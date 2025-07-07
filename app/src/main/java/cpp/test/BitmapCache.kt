738c9c0cc21de24774715faac36f3ce8f1c658d7
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-08 00:25:53 +0700

    Update BitmapCache.kt

--- app/src/main/java/cpp/test/BitmapCache.kt
+++ app/src/main/java/cpp/test/BitmapCache.kt
@@ -3,12 +3,7 @@ package cpp.test
 import android.graphics.Bitmap
 import android.util.LruCache
 
-object BitmapCache : LruCache<String, Bitmap>(getCacheSize()) {
-    private fun getCacheSize(): Int {
-        val maxMemory = (Runtime.getRuntime().maxMemory() / 1024).toInt()
-        return maxMemory / 8  // Using 1/8th of available memory
-    }
-
+object BitmapCache : LruCache<String, Bitmap>((Runtime.getRuntime().maxMemory() / 1024 / 8).toInt()) {
     override fun sizeOf(key: String, bitmap: Bitmap): Int {
         return bitmap.byteCount / 1024
     }
