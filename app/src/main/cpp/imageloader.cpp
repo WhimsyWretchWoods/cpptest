8fe4a5668dfd0d464d94ba11005914060769f4c6
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-07 19:45:08 +0700

    Update imageloader.cpp

--- app/src/main/cpp/imageloader.cpp
+++ app/src/main/cpp/imageloader.cpp
@@ -3,6 +3,6 @@
 
 extern "C"
 JNIEXPORT void JNICALL
-Java_app_breeze_ImageLoader_nativeInit(JNIEnv* env, jobject thiz) {
+Java_cpp_test_ImageLoader_nativeInit(JNIEnv* env, jobject thiz) {
     __android_log_print(ANDROID_LOG_INFO, "ImageLoader", "Native image loader initialized");
 }
