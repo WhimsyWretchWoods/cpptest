b37fbd9b59b7ddfe7eb17ec4b0a63474cec2bae5
WhimsyWretchWoods <riko.e610@gmail.com>
2025-07-07 19:06:45 +0700

    Create imageloader.cpp

---
+++ app/src/main/cpp/imageloader.cpp
@@ -0,0 +1,8 @@
+#include <jni.h>
+#include <android/log.h>
+
+extern "C"
+JNIEXPORT void JNICALL
+Java_app_breeze_ImageLoader_nativeInit(JNIEnv* env, jobject thiz) {
+    __android_log_print(ANDROID_LOG_INFO, "ImageLoader", "Native image loader initialized");
+}
